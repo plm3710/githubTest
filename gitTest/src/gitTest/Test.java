73e21b1f796f40128048dae99f967f546c8d27f6
audgh <audgh@MoungHo>
2017-10-18 14:42:48 +0900

    음...

--- gitTest/src/gitTest/Test.java
+++ gitTest/src/gitTest/Test.java
@@ -4,5 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("dddd");
 		System.out.println("코드수정");
+		System.out.println("난 안그럴꺼야");
+		System.out.println("수정해봅시다");
 	}
 }
